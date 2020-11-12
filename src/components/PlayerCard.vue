2d29356290a9a1f91d2ecff94bf2f507c951d0be
hutamy <hutamytriesthi@gmail.com>
2020-11-12 23:17:47 +0700

    Add component on GameRoom

---
+++ src/components/PlayerCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="player-card">
+    <div class="score">
+          10
+    </div>
+    <div class="player-name">
+        <h2>Sikomo</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlayerCard'
+}
+</script>
+
+<style>
+
+</style>
